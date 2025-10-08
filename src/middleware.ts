fdbcae3fe2f8d7c296744a2899be1cf11b645fc4
iqbalzayn01 <iqbalzayyanabiyyu@gmail.com>
2025-10-08 21:49:26 +0700

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,48 +1,30 @@
 import { NextResponse, type NextRequest } from 'next/server';
 import { auth } from './lib/auth';
-import { Role } from '@prisma/client';
 
 export default auth(async function middleware(req: NextRequest) {
   const session = await auth();
   const user = session?.user;
-  const userRole = user?.role;
-
   const pathname = req.nextUrl.pathname;
-  const url = req.nextUrl.clone();
 
-  const publicRoutes = ['/', '/threads', '/not-found', '/api'];
-  const authRoutes = ['/login', '/signup'];
+  const publicRoutes = ['/', '/login', '/signup', '/threads'];
 
-  const isPublicRoute = publicRoutes.some((route) => {
-    const pattern = new RegExp(`^${route}(\/|$)`);
-    return pattern.test(pathname);
-  });
+  const isPublicRoute = publicRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
 
-  const isAuthRoute = authRoutes.some((route) => {
-    const pattern = new RegExp(`^${route}(\/|$)`);
-    return pattern.test(pathname);
-  });
-
-  if (!user) {
-    if (isPublicRoute || isAuthRoute) {
-      return NextResponse.next();
-    }
-
+  if (!user && !isPublicRoute) {
+    const url = req.nextUrl.clone();
     url.pathname = '/login';
     return NextResponse.redirect(url);
   }
 
-  if (user) {
-    if (isAuthRoute) {
-      url.pathname = '/';
-      return NextResponse.redirect(url);
-    }
-
-    if (pathname.startsWith('/dashboard') && userRole !== Role.ADMIN) {
-      url.pathname = '/';
-      return NextResponse.redirect(url);
-    }
-    return NextResponse.next();
+  if (
+    user &&
+    (pathname.startsWith('/login') || pathname.startsWith('/signup'))
+  ) {
+    const url = req.nextUrl.clone();
+    url.pathname = '/';
+    return NextResponse.redirect(url);
   }
 
   return NextResponse.next();
@@ -50,6 +32,6 @@ export default auth(async function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
